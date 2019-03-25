332694b5f067a20ca8d4bc6bcd641c5571d33af4
Ashish Chahal <ashish.chahal94@gmail.com>
2019-03-25 22:09:21 +0530

    controlled input in react

---
+++ src/components/mainComponent.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react';
+
+class Main extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            value: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        this.setState({
+            value: event.target.value
+        })
+    }
+    render(){
+        return(
+        <div>
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+            <p>The answer is :</p>
+            <h1> {this.state.value}</h1>
+        </div>)
+    }
+}
+
+export default Main;
